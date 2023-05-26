05e09214bf3631100a303081640d0388b46d7728
Mohit Anand <itsmohitanand@gmail.com>
2023-05-26 10:15:59 +0200

    moving code

---
+++ dff/PixelDiffusion.py
@@ -0,0 +1,137 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from .DenoisingDiffusionProcess import *
+
+
+class PixelDiffusion(pl.LightningModule):
+    def __init__(
+        self,
+        generated_channels,
+        train_dataset=None,
+        valid_dataset=None,
+        num_timesteps=1000,
+        batch_size=1,
+        lr=1e-3,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        self.train_dataset = train_dataset
+        self.valid_dataset = valid_dataset
+        self.lr = lr
+        self.batch_size = batch_size
+
+        self.model = DenoisingDiffusionProcess(num_timesteps=num_timesteps, generated_channels=generated_channels)
+
+    @torch.no_grad()
+    def forward(self, *args, **kwargs):
+        return self.output_T(self.model(*args, **kwargs))
+
+    def input_T(self, input):
+        # By default, let the model accept samples in [0,1] range, and transform them automatically
+        return (input.clip(0, 1).mul_(2)).sub_(1)
+
+    def output_T(self, input):
+        # Inverse transform of model output from [-1,1] to [0,1] range
+        return (input.add_(1)).div_(2)
+
+    def training_step(self, batch, batch_idx):
+        images = batch
+        loss = self.model.p_loss(self.input_T(images))
+
+        self.log("train_loss", loss)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        images = batch
+        loss = self.model.p_loss(self.input_T(images))
+
+        self.log("val_loss", loss)
+
+        return loss
+
+    def train_dataloader(self):
+        if self.train_dataset is not None:
+            return DataLoader(
+                self.train_dataset, batch_size=self.batch_size, shuffle=True
+            )
+        else:
+            return None
+        
+    def val_dataloader(self):
+        if self.valid_dataset is not None:
+            return DataLoader(
+                self.valid_dataset,
+                batch_size=self.batch_size,
+                shuffle=False
+            )
+        else:
+            return None
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(
+            list(filter(lambda p: p.requires_grad, self.model.parameters())), lr=self.lr
+        )
+
+
+class PixelDiffusionConditional(PixelDiffusion):
+    def __init__(
+        self,
+        generated_channels,
+        condition_channels,
+        train_dataset=None,
+        valid_dataset=None,
+        batch_size=1,
+        lr=1e-3,
+    ):
+        pl.LightningModule.__init__(self)
+        self.save_hyperparameters()
+        self.generated_channels = generated_channels
+        self.condition_channels = condition_channels
+        self.batch_size = batch_size
+        self.lr = lr
+        self.train_dataset = train_dataset
+        self.valid_dataset = valid_dataset
+        self.lr = lr
+        self.batch_size = batch_size
+
+        self.model = DenoisingDiffusionConditionalProcess(
+            generated_channels=generated_channels,
+            condition_channels=condition_channels
+        )
+
+    @torch.no_grad()
+    def forward(self, batch, *args, **kwargs):
+        input, _, _ = batch
+        return self.output_T(self.model(self.input_T(input), *args, **kwargs))
+    
+    def training_step(self, batch, batch_idx):
+        input, output, _ = batch
+        loss = self.model.p_loss(self.input_T(output), self.input_T(input))
+
+        self.log("train_loss", loss)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        input, output, _ = batch
+        loss = self.model.p_loss(self.input_T(output), self.input_T(input))
+
+        self.log("val_loss", loss)
+
+        return loss
+
+    def config(self):
+        cfg = {"model_name":"PixelDiffusionConditional",
+                "model_hparam":{
+                    "generated_channels":self.generated_channels,
+                    "condition_channels":self.condition_channels,
+                    "batch_size":self.batch_size,
+                    "lr":self.lr,
+                }   
+                }
+        return cfg
